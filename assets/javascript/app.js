ea86916083cf2b5255582170bd9a1f1717c73ff3
Miss Araquel <missaraquel@gmail.com>
2018-07-05 15:53:39 -0700

    modified index.html and app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+    var options = [
+        
+        {
+            question: " What is Rihanna's real name? ",
+            choice: ["Ryder", "Rain", "Rachel", "Robyn"],
+            answer: 3,
+            photo: "assets/images/"
+        },
+        {
+            question: " What island is Rihanna from?",
+            choice: ["Jamaica", "Barbados", "Puerto Rico ", "Dominican Repubic"],
+            answer: 1,
+            photo: "assets/images/"
+        },
+        {
+            question: " What is Rihanna's first studio album titled?",
+            choice: ["Anti", "Good Girl Gone Bad", "Unapologetic", "Music Of The Sun"],
+            answer: 3,
+            photo: "assets/images/"
+        },
+        {
+            question: " Which artist has Rihanna NOT collaborated with? ",
+            choice: ["Beyonce", "Calvin Harris", "Jay-Z", "Coldplay"],
+            answer: 0,
+            photo: "assets/images/"
+        },
+        {
+            question: " What is Rihanna's makeup line named?",
+            choice: ["Rihanna Beauty", "Makeup by Rihanna", "Fenty Beauty", "Beauty by Rihanna"],
+            answer: 2,
+            photo: "assets/images/"
+        },
+        {
+            question: " Which shoe brand is Rihanna the creative director of? ",
+            choice: ["Adidas", "Puma", "Nike", "Skechers"],
+            answer: 1,
+            photo: "assets/images/"
+        },
+        {
+            question: " How many Grammy awards has Rihanna won? ",
+            choice: ["3", "6", "9", "8"],
+            answer: 2,
+            photo: "assets/images/"
+        
+
+    
+        }];
